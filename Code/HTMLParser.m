00e0e0db3db9659f5c63199d51d57bc0b20cc204
Chris Williams <chwilliam@gmail.com>
2013-08-16 16:59:10 -0400

    Added some parser shortcuts

--- Code/HTMLParser.m
+++ Code/HTMLParser.m
@@ -124,12 +124,27 @@ static inline NSString * NSStringFromHTMLInsertionMode(HTMLInsertionMode mode)
     BOOL _fragmentParsingAlgorithm;
 }
 
++ (HTMLDocument *)documentForString:(NSString *)string
+{
+	return [[self parserForString:string] document];
+}
+
++ (instancetype)parserForString:(NSString *)string
+{
+	return [[self alloc] initWithString:string];
+}
+
 - (id)initWithString:(NSString *)string
 {
     if (!(self = [self init])) return nil;
     _tokenizer = [[HTMLTokenizer alloc] initWithString:string];
     _tokenizer.parser = self;
     return self;
+}
+
++ (instancetype)parserForString:(NSString *)string context:(HTMLElementNode *)context
+{
+	return [[self alloc] initWithString:string context:context];
 }
 
 - (id)initWithString:(NSString *)string context:(HTMLElementNode *)context
