17cf7d065d80302be0efd6d41406441c1a8b5c16
Nolan Waite <nolan@nolanw.ca>
2013-11-15 23:02:44 -0400

    Speed up tokenizing character entities.
    - Binary search the win1252 replacement table.
    - Only search part of the named reference table for the longest prefix match.
    - Entity Fetcher utility downloads and formats the latest entity list from
      WHATWG.

---
+++ Utilities/Entity Fetcher/EntityFetcher.m
@@ -0,0 +1,91 @@
+//  EntityFetcher.m
+//
+//  Public domain. https://github.com/nolanw/HTMLReader
+
+#import <Foundation/Foundation.h>
+
+@interface Entity : NSObject
+
+@property (copy, nonatomic) NSString *name;
+@property (copy, nonatomic) NSArray *codepoints;
+
+@end
+
+@implementation Entity
+
+- (NSString *)description
+{
+    NSMutableString *description = [NSMutableString new];
+    [description appendFormat:@"{ @\"%@\", @\"", [self.name substringFromIndex:1]];
+    for (NSNumber *codepoint in self.codepoints) {
+        unsigned int c = codepoint.unsignedIntValue;
+        switch (c) {
+            case '"':
+                [description appendString:@"\\\""];
+                break;
+            case '\\':
+                [description appendString:@"\\\\"];
+                break;
+            case '\n':
+                [description appendString:@"\\n"];
+                break;
+            case '\t':
+                [description appendString:@"\\t"];
+                break;
+            default:
+                if (is_universal(c)) {
+                    [description appendFormat:@"\\U%08x", c];
+                } else {
+                    [description appendFormat:@"%C", (unichar)c];
+                }
+                break;
+        }
+    }
+    [description appendString:@"\" },"];
+    return description;
+}
+
+static inline BOOL is_universal(unsigned int c)
+{
+    // ISO C99 http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1256.pdf
+    return c == '$' || c == '@' || c == '`' || (c >= 0xA0 && (c < 0xD800 || c > 0xDFFF));
+}
+
+@end
+
+int main(void) { @autoreleasepool
+{
+    static NSString * const EntitiesURL = @"http://www.whatwg.org/specs/web-apps/current-work/multipage/entities.json";
+    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:EntitiesURL]];
+    if (!data) {
+        NSLog(@"could not download entities JSON from %@", EntitiesURL);
+        return 1;
+    }
+    NSError *error;
+    NSDictionary *entitiesJSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    if (!entitiesJSON) {
+        NSLog(@"could not decode entities JSON: %@", error);
+        return 1;
+    }
+    NSMutableArray *entities = [NSMutableArray new];
+    NSUInteger longestLength = 0;
+    for (NSString *name in entitiesJSON) {
+        if (name.length > longestLength) {
+            longestLength = name.length;
+        }
+        Entity *entity = [Entity new];
+        entity.name = name;
+        NSDictionary *replacement = entitiesJSON[name];
+        entity.codepoints = replacement[@"codepoints"];
+        [entities addObject:entity];
+    }
+    [entities sortUsingComparator:^(Entity *a, Entity *b) {
+        return [a.name compare:b.name];
+    }];
+    for (Entity *entity in entities) {
+        printf("%s\n", entity.description.UTF8String);
+    }
+    // -1 for the ampersand we ignore.
+    printf("\nstatic const NSUInteger LongestReferenceNameLength = %tu;\n", longestLength - 1);
+    return 0;
+} }
