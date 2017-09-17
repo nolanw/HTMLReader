ab9650c9185c05aa88db76c8f6154ac76765796d
Nolan Waite <nolan@nolanw.ca>
2017-09-17 17:10:17 -0300

    Fix HTMLDocument tests on Xcode 6.
    The NS*StringEncoding constants were apparently marked int, not NSStringEncoding?

--- HTMLReaderTests/HTMLDocumentTests.m
+++ HTMLReaderTests/HTMLDocumentTests.m
@@ -14,27 +14,27 @@
 - (void)testParsedStringEncodingUnspecified
 {
     HTMLDocument *document = [HTMLDocument new];
-    XCTAssertEqual(document.parsedStringEncoding, NSUTF8StringEncoding);
+    XCTAssertEqual(document.parsedStringEncoding, (NSStringEncoding)NSUTF8StringEncoding);
 }
 
 - (void)testParsedStringEncodingEmptyDocument
 {
     HTMLDocument *document = [HTMLDocument documentWithData:[NSData data] contentTypeHeader:nil];
-    XCTAssertEqual(document.parsedStringEncoding, NSWindowsCP1252StringEncoding);
+    XCTAssertEqual(document.parsedStringEncoding, (NSStringEncoding)NSWindowsCP1252StringEncoding);
 }
 
 - (void)testParsedStringEncodingContentTypeISOLatin1
 {
     HTMLDocument *document = [HTMLDocument documentWithData:(NSData *)[@"<!doctype html><h1>Hello!" dataUsingEncoding:NSUTF8StringEncoding]
                                           contentTypeHeader:@"text/html; charset=iso-8859-1"];
-    XCTAssertEqual(document.parsedStringEncoding, NSWindowsCP1252StringEncoding);
+    XCTAssertEqual(document.parsedStringEncoding, (NSStringEncoding)NSWindowsCP1252StringEncoding);
 }
 
 - (void)testParsedStringEncodingContentTypeUTF8
 {
     HTMLDocument *document = [HTMLDocument documentWithData:(NSData *)[@"<!doctype html><h1>Hello!" dataUsingEncoding:NSUTF8StringEncoding]
                                           contentTypeHeader:@"text/html; charset=utf-8"];
-    XCTAssertEqual(document.parsedStringEncoding, NSUTF8StringEncoding);
+    XCTAssertEqual(document.parsedStringEncoding, (NSStringEncoding)NSUTF8StringEncoding);
 }
 
 @end
